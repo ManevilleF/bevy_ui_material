f68ce3e46101f32083c7c08d68f14bcb76bfe53a
Felix de Maneville <felix.maneville@gmail.com>
2022-01-14 13:18:57 +0100

    Added badges

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
+//! # bevy_ui_material
+//!
+//! Materials for bevy UI Nodes
+//!
+//! [![workflow](https://github.com/ManevilleF/bevy_ui_material/actions/workflows/rust.yaml/badge.svg)](https://github.com/ManevilleF/bevy_ui_material/actions/workflows/rust.yaml)
+//!
+//! [![MIT licensed](https://img.shields.io/badge/license-MIT-blue.svg)](./LICENSE)
+//! [![unsafe forbidden](https://img.shields.io/badge/unsafe-forbidden-success.svg)](https://github.com/rust-secure-code/safety-dance/)
+//!
 // #![forbid(missing_docs)]
 #![forbid(unsafe_code)]
 #![warn(
